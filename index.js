c936d504df29affd64c13d8cc90eabd475e3dfe9
Stanley Huang <infamous_godhand@yahoo.com>
2020-07-26 12:32:57 +1000

    some test data set

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const tf = require("@tensorflow/tfjs-node");
+
+const features = tf.tensor(
+  [-121, 47],
+  [-121.2, 46.5],
+  [-122, 46.4],
+  [-120.9, 46.7]
+);
+const labels = tf.tensor([[200], [250], [215], [240]]);
