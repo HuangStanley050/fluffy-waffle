c80ab72e00dfc2451190ee310c81c756256eb128
Stanley Huang <infamous_godhand@yahoo.com>
2020-07-27 16:45:06 +1000

    issues

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
-
+const knn = (features, label, predictionPoint, k) => {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => {
+        return a[0] > b[0] ? 1 : -1;
+      })
+      .slice(0, k)
+      .reduce((acc, pair) => {
+        return acc + pair[1];
+      }, 0) / k
+  );
+};
 let { features, labels, testFeatures, testLabels } = loadCSV(
   "kc_house_data.csv",
   {
@@ -11,7 +29,14 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
     labelColumns: ["price"],
   }
 );
-
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+//testFeatures = tf.tensor(testFeatures);
+//testLabels = tf.tensor(testLabels);
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+console.log(result);
+// console.log(testFeatures);
+// console.log(testLabels);
 // const k = 2;
 // const features = tf.tensor(
 //   [-121, 47],
