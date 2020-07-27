41accf7eff61fc883dbd876c44e920e1a96566ac
Stanley Huang <Stanley.X.Huang@iag.com.au>
2020-07-27 10:23:37 +1000

    start with knn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const features = tf.tensor(
   [-120.9, 46.7]
 );
 const labels = tf.tensor([[200], [250], [215], [240]]);
+const predictionPoint = tf.tensor([-121, 47]);
+
+features.sub(predictionPoint).pow(2).sum(1).pow(0.5);
